e1f57fd89b7767d8891b158483e6d25bff6c8c53
CastroSDavi <daviscastro2002@gmail.com>
2022-09-06 21:13:07 -0300

    commit

--- forca/__init__.py
+++ forca/__init__.py
@@ -1,5 +1,10 @@
 from classe_forca import Forca
 
+print('-' * 30)
+print(f'{"Jogo da Forca": ^30}' )
+print('-' *30 )
 
-forca = Forca()
+palavra = str(input('Digite uma palavra: ')).lower()
+vidas =  int(input('Digite o número de vidas:'))
+forca = Forca(palavra, vidas)
 forca.jogar()
--- forca/classe_forca.py
+++ forca/classe_forca.py
@@ -1,47 +1,28 @@
 class Forca:
 
-    def __init__(self):
-        self.palavra = None
-        self.erros = None
+    def __init__(self, palavra: str, erros: int ):
+        self.palavra = palavra
+        self.erros = erros
+        self.palavra_apoio = list('-' for _ in range(len(self.palavra)))
 
     @staticmethod
-    def __introducao():
-        print('-' * len('jogo da forca'))
-        print('jogo da forca'.upper())
-        print('-' * len('jogo da forca'))
+    def __inserir_letra() -> str:
+        letra_escolhida = str(input('\nDigite uma letra:')).lower()
+        return letra_escolhida
 
-    def __inserir_palavra(self):
-        palavra = str(input('\nDigite uma palavra:'))
-        self.palavra = palavra
-
-    def __definir_numero_erros(self):
-        erros = int(input('Digite o número máximo de erros:'))
-        self.erros = erros
+    def __procurar_letra(self, letra_escolhida):
+        if letra_escolhida in self.palavra:
+            for contador, letra in enumerate(self.palavra):
+                if letra_escolhida == letra:
+                    self.palavra_apoio[contador] = letra
+                    print(''.join(self.palavra_apoio))
+        else:
+            self.erros -= 1
+    
+    def __verificar_resultado(self):
+        pass
 
     def jogar(self):
-        self.__introducao()
-        self.__inserir_palavra()
-        self.__definir_numero_erros()
-        contador_erros = 0
-        palavra_apoio = list('-' for _ in range(len(self.palavra)))
-        print('')
-        print(''.join(palavra_apoio))
-
+        print(''.join(self.palavra_apoio))
         while True:
-            letra_escolhida = str(input('\nDigite uma letra:'))
-            if letra_escolhida in self.palavra:
-                for contador, letra in enumerate(self.palavra):
-                    if letra == letra_escolhida:
-                        palavra_apoio[contador] = letra
-                print('')
-                print((''.join(palavra_apoio)))
-            else:
-                contador_erros += 1
-                if contador_erros == self.erros:
-                    print("Você, perdeu")
-                    break
-
-
-forca = Forca()
-
-forca.jogar()
+            self.__procurar_letra(self.__inserir_letra())
