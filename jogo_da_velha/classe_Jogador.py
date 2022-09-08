565750951bf046b83372cd964d3169e0d6fbcbd1
CastroSDavi <daviscastro2002@gmail.com>
2022-09-07 23:14:29 -0300

    commit

---
+++ jogo_da_velha/classe_Jogador.py
@@ -0,0 +1,20 @@
+class Jogador:
+    def __init__(self, simbolo, pontuacao = 0) -> None:
+        self.__simbolo = simbolo
+        self.__pontuacao = pontuacao 
+    
+    @property
+    def simbolo(self):
+        return self.__simbolo
+    
+    @simbolo.setter
+    def simbolo(self, valor):
+        self.__simbolo = valor
+
+    @property
+    def pontuacao(self):
+        return self.__pontuacao
+    
+    @pontuacao.setter
+    def pontuacao(self,valor):
+        self.__pontuacao = valor
