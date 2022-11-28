c6afd00febe9a9d004d6bccdfc0842771631fcd7
CastroSDavi <daviscastro2002@gmail.com>
2022-11-27 23:05:28 -0300

    verificando a letra escolhida

--- forca/classe_forca.py
+++ forca/classe_forca.py
@@ -5,10 +5,26 @@ class Forca:
         self.erros = erros
         self.palavra_apoio = list('-' for _ in range(len(self.palavra)))
 
-    @staticmethod
-    def __inserir_letra() -> str:
-        letra_escolhida = str(input('\nDigite uma letra:')).lower()
-        return letra_escolhida
+    def __escolher_letra(self):
+        while True:
+            letra_escolhida = str(input('Digite uma letra: ')).lower()
+            if (self.__verificar_tamanho_da_letra(letra_escolhida) and self.__verificar_tipo(letra_escolhida)):
+                return letra_escolhida
+            
+            print('Digite novamente')
+    
+    def __verificar_tamanho_da_letra(self, letra_escolhida):
+        tamanho = int(len(letra_escolhida))
+        if tamanho > 1:
+            return False
+        
+        return True
+
+    def __verificar_tipo(self, letra_escolhida):
+        if not letra_escolhida.isalpha():
+            return False
+
+        return True
 
     def __procurar_letra(self, letra_escolhida):
         if letra_escolhida in self.palavra:
@@ -18,11 +34,14 @@ class Forca:
                     print(''.join(self.palavra_apoio))
         else:
             self.erros -= 1
-    
-    def __verificar_resultado(self):
-        pass
 
     def jogar(self):
         print(''.join(self.palavra_apoio))
         while True:
-            self.__procurar_letra(self.__inserir_letra())
+            self.__procurar_letra(self.__escolher_letra())
+            if not '-' in self.palavra_apoio:
+                print('Parabéns, você ganhou')
+                break
+            elif self.erros == 0:
+                print('Que pena, você perdeu')
+                break
