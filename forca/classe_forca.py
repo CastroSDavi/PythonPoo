e1198725c85cd0f16e3dde1c2638d67fbc029e46
CastroSDavi <daviscastro2002@gmail.com>
2022-12-03 21:13:57 -0300

    adicionando type annotations

--- forca/classe_forca.py
+++ forca/classe_forca.py
@@ -3,9 +3,9 @@ class Forca:
     def __init__(self, palavra: str, erros: int):
         self.palavra = palavra
         self.erros = erros
-        self.palavra_apoio = list('-' for _ in range(len(self.palavra)))
+        self.palavra_apoio:list[str] = list('-' for _ in range(len(self.palavra)))
 
-    def __escolher_letra(self):
+    def __escolher_letra(self) -> str:
         while True:
             letra_escolhida = str(input('Digite uma letra: ')).lower()
             if self.__verificar_tamanho_da_letra(letra_escolhida) and self.__verificar_tipo(letra_escolhida):
@@ -13,7 +13,7 @@ class Forca:
 
             print('Digite novamente')
 
-    def __verificar_tamanho_da_letra(self, letra_escolhida):
+    def __verificar_tamanho_da_letra(self, letra_escolhida: str) -> bool:
         tamanho = int(len(letra_escolhida))
         if tamanho > 1:
             print('Digite apenas uma letra')
@@ -21,14 +21,14 @@ class Forca:
 
         return True
 
-    def __verificar_tipo(self, letra_escolhida):
+    def __verificar_tipo(self, letra_escolhida: str) -> bool:
         if not letra_escolhida.isalpha():
             print('Digite uma letra')
             return False
 
         return True
 
-    def __procurar_letra(self, letra_escolhida):
+    def __procurar_letra(self, letra_escolhida: str) -> None:
         if letra_escolhida in self.palavra:
             for contador, letra in enumerate(self.palavra):
                 if letra_escolhida == letra:
@@ -37,7 +37,7 @@ class Forca:
         else:
             self.erros -= 1
 
-    def __verificar_fim_de_jogo(self):
+    def __verificar_fim_de_jogo(self) -> bool:
         self.__procurar_letra(self.__escolher_letra())
         if not '-' in self.palavra_apoio:
             print('Parabéns, você ganhou')
@@ -49,7 +49,7 @@ class Forca:
 
         return False
 
-    def jogar(self):
+    def jogar(self) -> None:
         print(''.join(self.palavra_apoio))
         while True:
             self.__procurar_letra(self.__escolher_letra())
